a7677b803350a739a42fc812815adcae20820b5c
Roja <rahul@addtech-sw031.local>
2018-06-29 13:03:01 +0530

    Initial Commit

---
+++ Commit_intial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Commit_intial
+//
+//  Created by Rahul on 29/06/18.
+//  Copyright © 2018 AddTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Commit_intial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Commit_intial
+//
+//  Created by Rahul on 29/06/18.
+//  Copyright © 2018 AddTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
